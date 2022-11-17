aae7509227f39053b78c07d410ed3d717f7edec7
Seba-perseverance <scastrof2@outlook.com>
2022-11-17 03:55:12 -0300

    hola

--- Conectar.py
+++ Conectar.py
@@ -1,17 +1,27 @@
 from getpass import getpass
+from mysql.connector import connect, Error
 
 
 class DataBase:
-    def __init__(self,user,password):
-        self.user=user
-        self.password=password
-    
+
+    def __init__(self, user, password):
+        self.user = user
+        self.password = password
+
     def conectar(self):
         try:
             with connect(
                 host="localhost",
                 user=self.user,
                 password=self.password,
-                database="proyecto_nuevo1"
-
-            ) as connection
+                database="cinema"
+            ) as connection:
+                print(connection)
+                select = "SELECT * FROM person"
+                with connection.cursor() as cursor:
+                    cursor.execute(select)
+                    for row in cursor.fetchall():
+                        print(row)
+        except Error as e:
+            print(e)
+            
